8d8694b796abe11e5b059271cf9844eb8900a44f
ppethemxwatcher <ppeppe@gmail.com>
2016-03-21 23:30:44 +0900

    カテゴリ検索機能追加

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 import RealmSwift //追加
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
@@ -21,44 +21,77 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //日付近い順でソート：降順
     //以降内容をアップデートするとリスト内は自動的に更新される。
     let taskArray = try! Realm().objects(Task).sorted("date", ascending: false) //追加
+    var searchActive : Bool = false //課題追加
     var filteredArray = try! Realm().objects(Task).sorted("date", ascending: false) //課題追加
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchBar?.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-//MARK: UITableViewDataSourceプロトコルのメソッド
-//データの数（セルの数）を変えるメソッド
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return taskArray.count //追加
+
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true;
     }
     
-//各セルの内容を返すメソッド
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    //MARK: UITableViewDataSourceプロトコルのメソッド
+    //データの数（セルの数）を変えるメソッド
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       if(searchActive) {
+        return filteredArray.count
+       } else {
+        return taskArray.count //追加
+        }
+    }
+    
+    //各セルの内容を返すメソッド
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         //再利用可能なcellを得る
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
         
         //Cellに値を設定する
+        if(searchActive) {
+            let task = filteredArray[indexPath.row]
+            cell.textLabel?.text = task.title
+
+            let formatter = NSDateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm"
+            
+            let dateString:String = formatter.stringFromDate(task.date)
+            cell.detailTextLabel?.text = dateString
+            
+        } else {
         let task = taskArray[indexPath.row]
         cell.textLabel?.text = task.title
-        
+
         let formatter = NSDateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm"
         
         let dateString:String = formatter.stringFromDate(task.date)
         cell.detailTextLabel?.text = dateString
-        
+        }
         return cell
-    }
+        }
     
-//MARK: UITableViewDelegateプロトコルのメソッド
+    //MARK: UITableViewDelegateプロトコルのメソッド
     //各セルを選択した時に実行されるメソッド
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         performSegueWithIdentifier("cellSegue", sender: nil)//追加する
@@ -66,7 +99,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     //セルが削除可能なことを伝えるメソッド
     func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath)->UITableViewCellEditingStyle {
-    return UITableViewCellEditingStyle.Delete
+        return UITableViewCellEditingStyle.Delete
     }
     
     //Deleteボタンが押された時に呼ばれるメソッド
@@ -77,15 +110,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
             for notification in UIApplication.sharedApplication().scheduledLocalNotifications! {
                 if notification.userInfo!["id"] as! Int == task.id {
-                UIApplication.sharedApplication().cancelLocalNotification(notification)
-                
-                break
+                    UIApplication.sharedApplication().cancelLocalNotification(notification)
+                    
+                    break
                 }
             }
             
             //データベースから削除する
             try! realm.write{
-            self.realm.delete(self.taskArray[indexPath.row])
+                self.realm.delete(self.taskArray[indexPath.row])
                 tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
             }
         }
@@ -114,10 +147,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.reloadData()
     }
     
-//課題追加　検索 -- フィルタが効いてない
-    func searchBar(searchBar: UISearchBar, textDidChange searchText: String){
+    //課題追加　検索
+    func searchBar(searchBar: UISearchBar, textDidChange searchText:String){
         filteredArray = realm.objects(Task).filter("category contains '\(searchBar.text!)'").sorted("date", ascending: false)
         tableView.reloadData()
     }
 }
-
