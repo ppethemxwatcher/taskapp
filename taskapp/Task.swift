0dd1b96ff1d9bf570f9479b6a35dcf13afd9ba4d
ppethemxwatcher <ppeppe@gmail.com>
2016-03-21 11:38:18 +0900

    課題前

---
+++ taskapp/Task.swift
@@ -0,0 +1,34 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by swiftdev on 2016/03/19.
+//  Copyright © 2016年 kiyoko.ohashi. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用ID　プライマリーキー
+    dynamic var id = 0
+    
+//タイトル
+dynamic var title = ""
+    
+//カテゴリー ---課題
+dynamic var category = ""
+
+//内容
+dynamic var contents = ""
+
+//日時
+dynamic var date = NSDate()
+
+/**
+idをプライマリーキーとして設定
+*/
+
+override static func primaryKey() ->String? {
+return "id"
+}
+}
